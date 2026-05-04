ecc49d61745c61e8a8b0c150f1ae31a33beb976c
Sheikh Ishfaq <sheikhishfaq@gmail.com>
2026-05-04 10:44:11 +0530

    add,update delete get and get all

--- Backend/Controllers/productController.js
+++ Backend/Controllers/productController.js
@@ -1,28 +1,87 @@
-const mongoose =require("mongoose");
+const Product = require('../Models/Product');
 
-const Product = require("../Models/product")
-//add single product
-const addProduct = async (req, res) => {
-   console.log("BODY:", req.body);
-  try {
-    const product = new Product(req.body);
-    const savedProduct = await product.save(); 
-    res.status(201).json(savedProduct);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+const addProduct = async (req,res)=>{
+    try {
+        const {name, description, price, category} = req.body;
+        const image = req.file ? req.file.path : null;
+
+        if (!name || !description || !price || !category || ! image){
+            return res.status(400).json({error: "All properties are required"})
+        }
+        const product = await Product.create({name, description, price, category, image})
+        if (!product){
+            return res.status(400).json({error: "something went wrong please try again later"})
+        }
+
+
+        return res.status(201).json({message: "Product Added successfully"});
+    } catch (error) {
+        res.status(500).json({error})
+    }
 };
-//update single product
-router.put("/:id", async (req, res) => {
-  try {
-    const updatedProduct = await Product.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      { new: true }
-    );
-    res.json(updatedProduct);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-module.exports ={addProduct}
+//udate single produc
+const updatedProduct = async (req,res)=>{
+    try {const {name,price,description,category}=req.body;
+     const image = req.file ? req.file.path : undefined;
+ 
+
+    const updateData= {}
+    if(name !== undefined)updateData.name =name;
+     if(price !== undefined)updateData.price =price;
+      if(description !== undefined)updateData.description=description;
+       if(category!== undefined)updateData.category =category;
+        if(image !== undefined)updateData.image =image;
+         const Product= await Product.findByIdAndUpdate(req.params.id,updateData, { new: true, runValidators: true });
+         if( !Product){ return res.status(404).json({error:"product not updated"})};
+    
+       return res.status(200).json({message:"product updte successfully"})
+    
+    
+
+        
+    } catch (error) {res.status(500).json({error})
+        
+    }
+};
+//Deleted single product
+const deletedProduct = async (req,res)=>{
+    try {const id =req.params.id;
+        const Product = await Product.findByIdAndUpdate(id,
+            {isDeleted:true},
+
+           {new:true}
+        );
+        if(!Product)
+            return res.staus(404).json({message:"product not found"})
+           return res.status(200).json({message:" product deleted successfully"})
+
+        
+    } catch (error) {return res.status(500).json({error})
+        
+    }
+};
+//get single product
+const getProduct =async (req,res)=>{
+    try {
+        const {id}=req.params
+        const Product = await Product. findById(id);
+        if(!Product)
+            return res.status(404).json({message:"product not found"})
+            return res.status(200).json({message:"product fetched successfully"})
+        
+    } catch (error) {return res.status(500).json({error:error.message})
+        
+    }
+};
+const getAllProducts =async (req,res)=>{
+    try {
+        const Product = await Product.find();
+        if(!Products || Products.length===0)
+            return res.status(404).json({message:"product not found"})
+        return res.status(200).json({message:"product fetched successfully"})
+    } catch (error) { return res.status(500).json({error:error.message})
+        
+    }
+}
+
+module.exports = {addProduct,updatedProduct,deletedProduct,getProduct,getAllProducts}
--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,25 @@
-const express= require('express')
+const express= require('express');
+const path =require('path');
+const connectDB = require('./db/connectDB');
 
 const app = express();
-
+const PORT = 3000;
+const MONGO_URL = "mongodb://localhost:27017/e-commerce-web-app";
 
 //routes
 const productRoute = require('./Routes/productRoute');
 
+//middleware
+app.use(express.json());
+app.use("/uploads", express.static("uploads"));
+
 app.get('/', (req, res) => {
   res.send('Hello World')
 });
-app.use(express.json());
 app.use("/product",productRoute);
 
 
-app.listen(3000, () => {
+connectDB(MONGO_URL)
+app.listen(PORT, () => {
   console.log('Server is running on http://localhost:3000')
 })
