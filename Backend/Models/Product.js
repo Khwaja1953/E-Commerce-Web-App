45216ebd1a1492b5b9a3d0781307a8a884316dd3
rabiasayeed <rabiasayeed03@gmail.com>
2026-06-02 10:54:49 +0530

    added navbar footer and other pages

--- Backend/Models/Product.js
+++ Backend/Models/Product.js
@@ -65,4 +65,5 @@ const productSchema =new mongoose.Schema(
           timestamps: true,
     }
 );
-module.exports =mongoose.model("Product",productSchema)
+module.exports = mongoose.models.Product ||
+mongoose.model("Product",productSchema)
